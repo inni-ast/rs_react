76f1aa4411ef921966a69d7a04b61d47da3b1624
inni-ast <inni_@tut.by>
2023-11-02 16:54:22 +0300

    feat: add active class to nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,21 +1,24 @@
-import { useSearchParams } from 'react-router-dom';
+import { NavLink, useSearchParams } from 'react-router-dom';
 import styles from './nav.module.css';
 import './../../App.css';
-import { NavLink as Link } from 'react-router-dom';
 import { NavProps } from '../../types/types';
 
 export function Nav({ count, value }: NavProps) {
   const [searchParams] = useSearchParams();
   const page = searchParams.get('page') as string;
+
   const links = Array.from({ length: count }).map((el, i) => (
     <li key={i}>
-      <Link
+      <NavLink
         key={i}
         to={`?search=${value}&page=${i + 1}`}
-        className={`${styles.link} ${+page}===${i + 1}? ${styles.active}:''`}
+        className={styles.link}
+        style={({ isActive }) => ({
+          color: isActive && +page === i + 1 ? 'goldenrod' : 'aliceblue',
+        })}
       >
         {`Page ${i + 1}`}
-      </Link>
+      </NavLink>
     </li>
   ));
   return <nav className={styles.nav}>{links}</nav>;
--- src/components/main/Details.tsx
+++ src/components/main/Details.tsx
@@ -1,13 +1,18 @@
 import styles from './card.module.css';
 import { CardData } from '../../types/types';
 
-export function Card({ data }: CardData) {
+export function Details({ data }: CardData) {
+  console.log(data);
   return (
     <div className={styles.card}>
-      <p>Name: {data.name}</p>
+      Details
+      {/* <p>Name: {data.name}</p>
+      <p>Created{data.created}</p>
       <p>Climate: {data.climate}</p>
+      <p>Gravity: {data.gravity}</p>
       <p>Terrain: {data.terrain}</p>
       <p>Diameter: {data.diameter}</p>
+      <p>Population: {data.population}</p> */}
     </div>
   );
 }
